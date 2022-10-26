a89a511b2bf61a475b9021affdd779b3b5a6777d
Zubair <74538410+zubairbhuian@users.noreply.github.com>
2022-10-26 19:20:21 +0600

    form10.16.22

--- src/script.js
+++ src/script.js
@@ -94,3 +94,22 @@ $(function () {
 	});
 
 })
+
+
+// form
+const passwordToggle = document.querySelector('.js-password-toggle')
+
+passwordToggle.addEventListener('change', function() {
+  const password = document.querySelector('.js-password'),
+    passwordLabel = document.querySelector('.js-password-label')
+
+  if (password.type === 'password') {
+    password.type = 'text'
+    passwordLabel.innerHTML = 'hide'
+  } else {
+    password.type = 'password'
+    passwordLabel.innerHTML = 'show'
+  }
+
+  password.focus()
+})
